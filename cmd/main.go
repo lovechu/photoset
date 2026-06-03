5cfd87d57ca6babd83425527989f75c9c0501d54
lovechu <lovechu@honker.org>
2026-06-03 16:28:36 +0800

    Feat: AutoMigrate 新增 LoginHistory + UserDevice + UserPrivacySetting 三张表

--- cmd/main.go
+++ cmd/main.go
@@ -72,6 +72,10 @@ func main() {
 		&domain.OAuthClient{},
 		&domain.OAuthAuthorization{},
 		&domain.OAuthToken{},
+		// User security & privacy
+		&domain.LoginHistory{},
+		&domain.UserDevice{},
+		&domain.UserPrivacySetting{},
 	); err != nil {
 		// 忽略多对多关联表的重复主键错误（表已存在时 GORM 会尝试重复添加主键）
 		if !isMultiplePrimaryKeyError(err) {
